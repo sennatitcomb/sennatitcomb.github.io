3ea3424d6c05157b915b892945c2eb33b3afe540
Senna <sennatitcomb@gmail.com>
2023-11-04 11:54:38 -0700

    added to index.html

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // script.js
+const skillsSection = document.getElementById('skills');
+const skillsList = document.querySelector('.skills-list');
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            skillsSection.classList.add('in-view');
+        }
+    });
+});
+
+observer.observe(skillsSection);
