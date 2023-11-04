ca8672a9448c78c474cc9bb8b08c578c865eae2e
Senna <sennatitcomb@gmail.com>
2023-11-04 11:45:48 -0700

    added to index.html

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
 // script.js
-
-const rainbow = document.getElementById('rainbow');
-
-document.addEventListener('mousemove', (e) => {
-    const x = e.clientX;
-    const y = e.clientY;
-    rainbow.style.left = `${x}px`;
-    rainbow.style.top = `${y}px`;
-});
